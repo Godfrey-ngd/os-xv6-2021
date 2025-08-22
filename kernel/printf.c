94dc8695aa4509a764895bac88daa54d30785308
Godfrey-ngd <NGD18256249656@outlook.com>
2025-08-22 14:38:42 +0800

    cow

--- kernel/printf.c
+++ kernel/printf.c
@@ -121,7 +121,6 @@ panic(char *s)
   printf("panic: ");
   printf(s);
   printf("\n");
-  backtrace();
   panicked = 1; // freeze uart output from other CPUs
   for(;;)
     ;
@@ -132,14 +131,4 @@ printfinit(void)
 {
   initlock(&pr.lock, "pr");
   pr.locking = 1;
-}
-
-void
-backtrace(void)
-{
-  uint64 fp_address = r_fp();
-  while(fp_address != PGROUNDDOWN(fp_address)) {
-    printf("%p\n", *(uint64*)(fp_address-8));
-    fp_address = *(uint64*)(fp_address - 16);
-  }
 }
